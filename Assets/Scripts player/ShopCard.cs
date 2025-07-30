bab36d9df5478acde06fc7dc2cf27230c1daaab0
sopheagle4@gmail.com <>
2025-07-30 15:48:49 -0700

    player movement and dash | shop

---
+++ Assets/Scripts player/ShopCard.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public enum Rarity { Common, Rare, Epic, Legendary }
+
+[System.Serializable]
+
+public class ShopCard
+{
+    public string cardName;
+    public Rarity rarity;
+    public Sprite icon;
+}
